6218f5b0fcece3e8f6f0f69c1f15e2ab043a5409
Iulia Murtazina <iulia.murtazina@gmail.com>
2023-05-30 12:10:35 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import RootLayout from "./pages/Root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
